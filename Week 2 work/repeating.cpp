0d5859f6a4079282ee93af77ea8c301b9ccb96ba
mollyfeldman <feldman.molly@gmail.com>
2014-07-02 11:12:49 -0400

    organizational changes

---
+++ Week 2 work/repeating.cpp
@@ -0,0 +1,338 @@
+#include <iostream>
+using namespace std;
+
+int num = 0;
+const int p = 3; /*prime number 1 */
+const int q = 7; /*prime number 2 */
+const int l = 5;
+const int r = 1; /*additional value, our n is of the form pqr */
+
+<<<<<<< HEAD
+const maxf2 = 5;
+const maxf3 = 10;
+const maxf1 = maxf2 + maxf3 -1; 
+
+const maxrepf2 = 5;
+const maxrepf3 = 7;
+
+const int n = p*l*q*r;
+int g1[n], g2[n], g3[n], g4[n], f2[15], f3[21], f1[35],  F[n], repf2[5], repf2[7];
+=======
+const int maxf2 = 5;
+const int maxf3 = 10;
+const int maxf1 = maxf2 + maxf3 -1; 
+
+const int maxrepf2 = 5;
+const int maxrepf3 = 7;
+
+const int n = p*l*q*r;
+int g1[n], g2[n], g3[n], g4[n], f2[15], f3[21], f1[35],  F[n], repf2[5], repf3[7];
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+bool boolc[n][3];
+/*coefficient vectors, representing how we can generate the coefficients given our algorithms*/
+
+void printVec(int*, int);
+void printGaps(int*, int);
+void printAns();
+bool isPeriodic(int*, int);
+bool hasPeriod(int*, int, int);
+void setCoeffs();
+bool determineBool(int* , int*);
+void repeat(int*, int* , int, int);
+bool checkAlt(int*);
+void combine(int* , int* , int*);
+void finish(int);
+
+////////////////////////////////////////////////////////////////////////
+/* setCoeffs - basic setup for all coefficient vectors use within */
+void setCoeffs() {
+	for (int x=0; x < n; x++) {
+		for (int y=0; y <n ; y++ ){
+			boolc[x][y] = false;
+		}
+	}
+	for (int i=0; i<n; i++) g1[i] = g2[i] = g3[i] = F[i] = 0;
+<<<<<<< HEAD
+	for (int e=0; e<15; e++) f2[e] = 1;
+	for (int f=0; f<21; f++) f3[f] = 1;
+=======
+	for (int e=0; e<15; e++) f2[e] = 0;
+	for (int f=0; f<21; f++) f3[f] = 0;
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+	for (int m=0; m<5; m++) repf2[m] =1;
+	for (int n1=0; n1<7; n1++) repf3[n1] = 1;
+	//for (int h=0; h<35; h++) f1[h] = 0;
+}
+///////////////////////////////////////////////////////////////////////////
+/* repeat - repeats the coefficient sequence of v, with alternating sign,
+and sets them to be the coefficient sequence of w.
+	recursive definition
+*/
+void repeat(int* v, int* w, int num, int rep) {
+	int size1 = sizeof(v)/sizeof(int); 
+	int size2 = sizeof(w)/sizeof(int); //WATCH OUT; PRINT OUT, could give wrong results
+
+	if (rep == size2) {
+		return;
+	}
+
+	for (int i = rep; i < (size2/size1 + rep); i++ ) {
+		w[i] = num*v[i];
+
+	}
+	repeat(v, w, -num, rep+size1);
+
+}
+
+////////////////////////////////////////////////////////////////////////////
+/* determineBool - determines all possible coefficient values for g3[n]
+based on the fact that F[n] can only have coefficient values in -1, 1, or 0
+*/
+bool determineBool(int* v, int* w) {
+	
+	for (int i=0; i< n; i++) {
+		if (v[i] == w[i] == 1) {
+			boolc[i][0] = true; 
+			boolc[i][1] = boolc[i][2] = false;
+		}
+
+		else if ((v[i] == 0 && w[i] == 1) || (v[i] == 1 && w[i] == 0)) {
+			boolc[i][0] = boolc[i][1] = true;
+			boolc[i][2] = false;
+		}
+
+		else if ((v[i] == -1 && w[i] == 0) || (v[i] == 0 && w[i] == -1)) {
+			boolc[i][0] = false;
+			boolc[i][1] = boolc[i][2] = true;
+		}
+
+		else if ((v[i] == w[i] == -1)) {
+			boolc[i][0] = boolc[i][1] = false;
+			boolc[i][2] = true;
+		}
+
+		else if ((v[i] == w[i] == 0) || (v[i] == -1 && w[i] == 1) || (v[i] == 1 && w[i] == -1)) {
+			boolc[i][0] = boolc[i][1] = boolc[i][2] = true;
+		}
+
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////
+/* checkAlt - checks the necessary condition for F[n] that (a) it has an alternating sequence of -/+'s
+and (b) that it has an odd number [reduces to checking whether or not the last sign is +] */
+bool checkAlt(int* v) {
+	if (v[0] != 1) return false;
+	int sign = -1;
+	
+	for (int i=1; i<n; i++) {
+		if (v[i] == 0) continue;
+		if (v[i] == sign) return false;
+		if (v[i] == -sign) {
+			sign = -sign;
+		}
+	}
+	if (sign == -1) {
+		return false;
+	} else {
+	return true && (!isPeriodic(v, n));
+	}
+}
+//////////////////////////////////////////////////////////////////////////
+/* combine - adds three coefficient vectors to determine the final Reinhardt polynomial */
+void combine(int* v, int* w, int *x) {
+	for (int i=0; i<n; i++) {
+		F[i] = v[i] + w[i] + x[i];
+	}
+}
+
+//////////////////////////////////////////////////////////////
+/* finish - calculates all possible g1's from the boolean array, checks their
+/* validity as Reinhardt polynomials, and prints out any valid results */
+void finish(int start) {
+	//want the results to be equal to g1
+	if (start == n+1) {
+		combine(g1,g2,g3);
+		if (checkAlt(F)) printAns();
+	}
+
+	if (boolc[start][0]) {
+		g1[start] = -1;
+		finish(start+1);
+	}	
+
+	if (boolc[start][1]) {
+		g1[start] = 0;
+		finish(start+1);
+	}
+
+	if (boolc[start][2]) {
+		g1[start] = 1;
+		finish(start+1);
+	}
+
+}
+
+///////////////////////////////////////////////////////////////
+int main() {
+	
+	cout << "START EXECUTION" << endl;
+	//sets up the coefficient vectors
+	setCoeffs();
+<<<<<<< HEAD
+
+	while (true) {
+
+		while(true) {
+			printVec(repf2, 5);
+			cout << endl;
+			printVec(repf3, 7);
+			cout << endl;
+			cout << endl;
+=======
+	
+
+	
+	repeat(f2, g2, 1, 0);
+	repeat(f3, g3, 1, 0);
+	determineBool(g2, g3);
+	finish(0);
+	big:
+	while (true) {
+
+		while(true) {
+			//printVec(repf2, 5);
+			//cout << endl;
+			//printVec(repf3, 7);
+			//cout << endl;
+			//cout << endl;
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+	        repeat(repf2, f2, 1, 0);
+			repeat(repf3, f3, 1, 0);
+			repeat(f2, g2, 1, 0);
+			repeat(f3, g3, 1, 0);
+
+			determineBool(g2, g3);
+			finish(0);
+
+	       	// next iteration:
+	       	repf3[maxrepf3]--;
+<<<<<<< HEAD
+	       	for(int j= maxrepf3; j>1; j--) {  //decreasing
+=======
+	       	for(int j= maxrepf3; j>=1; j--) {  //decreasing
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+	       	   if (repf3[j] < -1) {
+	           	repf3[j-1]--;
+	     	   	repf3[j]=1;
+	        	}
+
+	        	if(repf3[0]<-1) {
+<<<<<<< HEAD
+	        	    break;
+	        	}
+	        }       	        	        	
+		}
+
+		repf2[maxrepf2]--;
+    	for(int k= maxrepf2; k>1; k--) {  //decreasing
+       		if (repf2[k] < -1) {
+          		repf2[k-1]--;
+     	  		repf2[k]=1;
+       		}
+
+       		if(repf2[0]<-1) {
+          		break;
+        	}
+    	}
+	}
+
+=======
+	        	    repf3[0] = 1;
+	        	    goto outside;
+
+	        	}
+	        }       	        	        	
+		}
+		outside:
+			repf2[maxrepf2]--;
+    		for(int k= maxrepf2; k>=1; k--) {  //decreasing
+       			if (repf2[k] < -1) {
+          			repf2[k-1]--;
+     	  			repf2[k]=1;
+       			}
+
+       			if(repf2[0]<-1) {
+       				repf2[0] =1;
+          			goto big;
+        		}
+    		}
+		}
+	
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+	cout << "hello" << endl;
+	return 0;
+}
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 1cefc428b253b1409875064123aa83f6d231adf9
+/****************************************************************/
+/* printAns, printGaps and printVec are simply print formatting functions */
+void printAns() {
+cout << num << " " << "a:";
+    printVec(f1, n);
+    cout << endl;
+    printVec(f2, n);
+    cout << endl;
+    printVec(f3, n);
+    cout << endl << endl;
+    printVec(F, n);
+    cout << endl;
+    printGaps(F, n);
+    cout << '\n' << endl;
+    num++;
+}
+
+
+void printGaps(int* v, int n) { /*gives us the actual "list sequence" of numbers for the Reinhardt angle differences*/
+    int k = 0, gap;
+    while (++k < n) {
+        gap = 0;
+        for (gap=0; k < n && v[k] == 0; k++) gap++;
+        cout << " " << (gap+1);
+    }
+}
+
+void printVec(int* v, int n) { /*gives us our valid sequence of plus and minuses to print out*/
+    for (int i=0; i<n; i++) {
+        if (v[i] == 1) cout << '+';
+        else if (v[i] == -1) cout << '-';
+        else cout << v[i];
+    }
+}
+/*************************************************************/
+/* returns relevant properties about the  model, once it has been generated */
+
+bool isPeriodic(int* c, int n) {
+    for (int t=2; t<=n/2; t++) {
+        if (hasPeriod(c, n, t)) return true;
+    }
+    return false;
+}
+
+bool hasPeriod(int* c, int n, int t) {
+    for (int i=0; i<n-t; i++) {
+        if (c[i] != -c[i+t]) return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
---
+++ Week 2 work/srch_hardcode.cpp
@@ -0,0 +1,160 @@
+/*/////////////////////////////////*/
+/* Michael's Code for generating sporadic Reinhardt Polygons, pqr method
+/*//////////////////////////////*/
+
+#include <iostream>
+using namespace std;
+
+int num = 0;
+const int p = 3; /*prime number 1 */
+const int q = 7; /*prime number 2 */
+const int r = 10; /*additional value, our n is of the form pqr */
+
+const int n = p*q*r;
+int a[n], b[n], c[n], f3[n]; /*coefficient vectors, representing how we can generate the coefficients given our algorithms*/
+
+/* function declaration, most important are set, testC, findB, enumA, and hasPeriod */
+void set(int*, int, int, int, int);
+void printVec(int*, int);
+void printGaps(int*, int);
+void printAns();
+bool testC();
+void findB(int, int);
+void enumA(int);
+bool isPeriodic(int*, int);
+bool hasPeriod(int*, int, int);
+
+
+/* runs the creation of the coefficient sequences*/
+int main() {
+    for (int i=0; i<n; i++) a[i] = b[i] = c[i] = f3[i] = 0;
+    set(a, n, q, 0, 1);
+    enumA(p);
+    return 0;
+}
+
+void set(int* v, int n, int q, int offset, int val) {
+    int m = n/q; /* integer division, why integer division? */
+    for (int j=0; j<q; j+=2) v[m*j+offset] = val; /*basically setting every other coefficient in a to 1 or -1 */
+    for (int j=1; j<q; j+=2) v[m*j+offset] = -val; 
+
+}
+
+void enumA(int i) { 
+    //cout << "in enumA" << endl;        /*weird nested loop structure; why is it like this? */
+    if (i == 0) {
+        findB(1, (n/p)/2);
+    } else {
+        set(a, n, q, i, 1);
+        enumA(i-1);
+        set(a, n, q, i, 0);
+        enumA(i-1);
+        set(a, n, q, i, -1);
+        enumA(i-1);
+         int k;
+    }
+}
+
+// prior is the last nonzero term in c.
+// k is the current index we need to set.
+void findB(int prior, int k) {
+    //cout << "in findB" << endl;
+    if (k == n/p) {
+        //cout << "printing" << endl;
+        if (testC()) printAns();
+    } else if (a[k] == prior) {
+        set(b, n, p, k, -prior);
+        findB(prior, k+1);
+    } else if (a[k] == -prior) {
+        set(b, n, p, k, 0);
+        findB(a[k], k+1);
+        set(b, n, p, k, prior);
+        findB(prior, k+1);
+    } else {  // a[k] == 0
+        set(b, n, p, k, 0);
+        findB(prior, k+1);
+        set(b, n, p, k, -prior);
+        findB(-prior, k+1);
+    }
+}
+
+bool testC() {
+    //HARDCODING IN f_3(x) = 1-z for n=210
+    f3[0]= 1;
+    //for (int j=0; j<n; j++) { cout << f3[j] << " ";}
+    //    cout << endl;
+
+    f3[1] = -1;
+    
+    f3[42] = -1;
+    f3[43] = 1;
+    f3[84] = 1;
+    f3[85] = -1;
+    f3[126] = -1;
+    f3[127] = 1;
+    f3[168] = 1;
+    f3[169] = -1;
+
+    for (int i=0; i<n; i++) c[i] = a[i] + b[i] + f3[n];
+    int prior = 1;
+
+    for (int i=1; i<n; i++) {
+        if (c[i]) { //if random weird cases happen, don't even run isPeriodic and return
+            if (c[i] == prior || c[i] > 1 || c[i] < -1) return false;
+            prior = c[i];
+        }
+    }
+    return !isPeriodic(c, n);
+}
+
+
+/****************************************************************/
+/* printAns, printGaps and printVec are simply print formatting functions */
+void printAns() {
+cout << num << " " << "a:" << endl;
+    printVec(a, n);
+    cout << endl;
+    printVec(b, n);
+    cout << endl;
+    printVec(f3,n);
+    cout << endl;
+    printVec(c, n);
+    cout << endl;
+    printGaps(c, n);
+    cout << '\n' << endl;
+    num++;
+}
+
+
+void printGaps(int* v, int n) { /*gives us the actual "list sequence" of numbers for the Reinhardt angle differences*/
+    int k = 0, gap;
+    while (++k < n) {
+        gap = 0;
+        for (gap=0; k < n && v[k] == 0; k++) gap++;
+        cout << " " << (gap+1);
+    }
+}
+
+void printVec(int* v, int n) { /*gives us our valid sequence of plus and minuses to print out*/
+    for (int i=0; i<n; i++) {
+        if (v[i] == 1) cout << '+';
+        else if (v[i] == -1) cout << '-';
+        else cout << v[i];
+    }
+}
+/*************************************************************/
+/* returns relevant properties about the  model, once it has been generated */
+
+bool isPeriodic(int* c, int n) {
+    for (int t=2; t<=n/2; t++) {
+        if (hasPeriod(c, n, t)) return true;
+    }
+    return false;
+}
+
+bool hasPeriod(int* c, int n, int t) {
+    for (int i=0; i<n-t; i++) {
+        if (c[i] != -c[i+t]) return false;
+    }
+    return true;
+}
